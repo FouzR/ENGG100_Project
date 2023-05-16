5f1114df2cc37bfeefe43f9908bcc6df67c0b023
Fouz Reda K <fouz290903@gmail.com>
2023-05-16 09:11:04 +0400

    Alright, lets see if this works

---
+++ Base.m
@@ -0,0 +1,9 @@
+% Beginning of our ENGG100 Project
+% 
+% Things we'll need to account for
+% 
+% Equations to run the simulation and to move the plot
+% 
+% Use an image on the graph and on the moving plot
+% 
+% needs sliders/controls to change tragectory
