982ada9c9ff540fe0aec707bf04f878bd27b46f3
Fouz Reda K <fouz290903@gmail.com>
2023-06-06 17:28:37 +0400

    Animation-ish has been added

--- GraphOfLengthCode.m
+++ GraphOfLengthCode.m
@@ -15,9 +15,44 @@ while ~(y(increment)>= r*sin(L/r)) || ~(X>=r-r*cos(L/r))
     increment = increment + 1;
     X = X + 0.01;
 end
+End = x(length(x));
+Slope_at_end = (50-End)/(sqrt(-End*(End-100)));
+YEnd = y(length(y));
+% disp([YEnd, Slope_at_end, End])
+for i = length(y):length(y)+20
+    x = [x, X];
+    y = [y, Slope_at_end*(x(i)-End)+YEnd];
+    X = X + 0.01;
 
-% p = plot(x,y);
-% xlim([-1 50]);
-% ylim([-1 50]);
+p = plot(x,y);
+xlim([-2 50]);
+ylim([-5 50]);
 Graphtest(x,y, 10,1)
+
+hold on
+p = plot(x(1),y(1),'o','MarkerFaceColor','red');
+hold off
+axis manual
+for k = 2:3:length(x)
+    p.XData = x(k);
+    p.YData = y(k);
+    drawnow
+end
+% i = 1;
+% while i < length(x)
+%     %// radius
+%     hold on
+%     Graphtest(x,y, 10,1)
+%     r = 1;
+%     
+%     %// center
+%     c = [x(i), y(i)];
+%     
+%     pos = [c-r 2*r 2*r];
+%     rectangle('Position',pos,'Curvature',[1 1])
+%     hold off
+%     i = i + 5;
+% end
+
+
 end
