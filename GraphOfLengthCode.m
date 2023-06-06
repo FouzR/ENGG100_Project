3d91a1ef69ff1afe8349b02dcc9b63de88a3a561
Fouz Reda K <fouz290903@gmail.com>
2023-06-06 17:00:04 +0400

    Fixed the final part of the plotting of the road with specific curvature

--- GraphOfLengthCode.m
+++ GraphOfLengthCode.m
@@ -1,20 +1,23 @@
 % R-changes
 % Length - 50m 
 % y = sqrt(2rx-x2)
-clear
-clc
-r = input('Enter Radius');
-L = input('Enter Road Length');
-
+% clear
+% clc
+% r = input('Enter Radius');
+% L = input('Enter Road Length');
+function GraphOfLengthCode(r,L)
 x = [0 0 0 0 0 0];y = [-5 -4 -3 -2 -1 0];
-X = 0.1;
+X = 0.01;
 increment = 6;
-while y(increment) <= r*sin(L/r) || 2*r>x(increment)
+while ~(y(increment)>= r*sin(L/r)) || ~(X>=r-r*cos(L/r))
     x = [x, X];
-    y = [y, sqrt(L*X-X^2)];
+    y = [y, sqrt(2*r*X-X^2)];
     increment = increment + 1;
-    X = X + 0.1;
+    X = X + 0.01;
 end
 
-
+% p = plot(x,y);
+% xlim([-1 50]);
+% ylim([-1 50]);
 Graphtest(x,y, 10,1)
+end
