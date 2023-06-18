8bb600c3845a05b984de5984786579cd674e7cf1
Fouz Reda K <fouz290903@gmail.com>
2023-06-18 21:33:59 +0400

    Fix G-force values

--- Gforcefriction.m
+++ Gforcefriction.m
@@ -1,13 +1,15 @@
-function [G,F] = Gforcefriction(v,r,m,bankAng)
+function [G,F] = Gforcefriction(v,r,m,bankAng, friction_on_tyres)
 g = 9.81;
 
     if bankAng < 90 && bankAng >= 0
         F = ((v^2/r)*m)/cosd(bankAng) + m*g*tand(bankAng);
 
-        G = ((v^2/r)+(m*g*sind(bankAng)))/g;
+        %G1 = ((v^2/r)+(m*g*sind(bankAng)))/g;
+        G = (friction_on_tyres*cosd(bankAng)+sind(bankAng))/(cosd(bankAng)-friction_on_tyres*sind(bankAng));
     else
         msgbox('Invalid Bank Angle')
         F = 0;
+        %G1 = 0;
         G = 0;
     end
 
