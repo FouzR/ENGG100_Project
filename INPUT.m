7b67f4e332236515fec96b265e79ade45b6b465f
Muaadh Shihabdeen <muaadh9@gmail.com>
2023-06-18 12:04:17 +0400

    repurposed input.m to get mass and tire type

--- INPUT.m
+++ INPUT.m
@@ -1,8 +1,14 @@
-function [Velocity, Radius_of_curvature] = INPUT
+function [m, coeffric] = INPUT
 % Mess = msgbox('This is a program that takes in Velocity and Radius of curvature and outputs the G-Forces, and if the tyres will slip of not'); 
 opts.Interpreter = 'tex';
-X = inputdlg({'Enter Velocity of the car(m/s^{2})', ...
-    'Enter Radius of curvature of the corner'}, 'Input', 1, {'0','0'}, opts);
-Velocity =  str2double(X{1});
-Radius_of_curvature = str2double(X{2});
+X = inputdlg({'Enter Mass of Car (kg)', ...
+    'Enter Desired Tire type (Soft/Hard)'}, 'Input', 1, {'0','0'}, opts);
+m =  str2double(X{1});
+    if strcmp(X{2},'hard') == 1
+        coeffric = 0.7;
+    elseif strcmp(X{2},'soft') == 1
+        coeffric = 0.9;
+    else
+        msgbox('Please input a valid tire type')
+    end
 end
