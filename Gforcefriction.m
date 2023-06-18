6c86b85a2811026a02707cb40fc429869984b829
Muaadh Shihabdeen <muaadh9@gmail.com>
2023-06-18 19:50:44 +0400

    added friction/bankangle and g-force/bankangle

--- Gforcefriction.m
+++ Gforcefriction.m
@@ -1,7 +1,7 @@
 function [G,F] = Gforcefriction(v,r,m,bankAng)
 g = 9.81;
 
-    if bankAng < 90 && bankAng > 0
+    if bankAng < 90 && bankAng >= 0
         F = ((v^2/r)*m)/cos(bankAng) + m*g*tan(bankAng);
 
         G = ((v^2/r)+(m*g*sin(bankAng)))/g;
