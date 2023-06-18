08508d25bfad877e4d07b57f81d56d450c40805c
ManuAugustine <135527836+ManuAugustine@users.noreply.github.com>
2023-06-18 21:35:51 +0400

    Add files via upload

--- Gforce.m
+++ Gforce.m
@@ -1,18 +1,12 @@
-function f = Gforce(v,r,turn) %the variables here are for angular speed, radius of curvature of the turn and t is for whether the car is turning or going in a straight path
+function G = Gforce(v,r,ang_bank) %the variables here are for angular speed, radius of curvature of the turn and t is for whether the car is turning or going in a straight path
 
 g=9.81; %Setting value for gravity
 
-if turn == 0 %G force on car travelling in a straight path
-    a=input('Enter acceleration of the car: ')
-    G=((g^2+a^2)^0.5)/g;
-    disp (['The G force acting on the vehicle is ',num2str(G),' g']);
-
-elseif turn == 1
-    ang_bank=input('Enter banking angle (degrees): '); %Taking input for the banking angle
-    G= ((g*(cos(ang_bank))^2)^2+((v^2/r)+(g*cos(ang_bank)*sin(ang_bank)))^2))^0.5/g %Calculating G force for car in Uniform circular motion
-    disp(['The G force acting on the vehicle is ',num2str(G),' g']);
+if r>=0
+    G= ((g*(cosd(ang_bank))^2)^2+((v^2/r)+(g*cosd(ang_bank)*sind(ang_bank)))^2)^0.5/g %Calculating G force for car in Uniform circular motion
+    
 
 else 
-    disp('Invalid input for turn');
+    disp('Invalid input');
 
 end
