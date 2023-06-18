9b99620ae352e2dd946f2f05e41423f4c921eed8
Fouz Reda K <fouz290903@gmail.com>
2023-06-18 21:22:24 +0400

    changed to the easier unit(degrees)

--- Gforcefriction.m
+++ Gforcefriction.m
@@ -2,9 +2,9 @@ function [G,F] = Gforcefriction(v,r,m,bankAng)
 g = 9.81;
 
     if bankAng < 90 && bankAng >= 0
-        F = ((v^2/r)*m)/cos(bankAng) + m*g*tan(bankAng);
+        F = ((v^2/r)*m)/cosd(bankAng) + m*g*tand(bankAng);
 
-        G = ((v^2/r)+(m*g*sin(bankAng)))/g;
+        G = ((v^2/r)+(m*g*sind(bankAng)))/g;
     else
         msgbox('Invalid Bank Angle')
         F = 0;
