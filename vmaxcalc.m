af50fa4b5163178c10446c0faf347497033d18c4
Fouz Reda K <fouz290903@gmail.com>
2023-06-19 15:29:18 +0400

    Fixes

--- vmaxcalc.m
+++ vmaxcalc.m
@@ -3,6 +3,6 @@ function vmax = vmaxcalc(r,ang_bank,TyreFriction)
 
 g=9.81;
 
-vmax=sqrt((r*g*(tand(ang_bank)-TyreFriction)/(1+TyreFriction*tand(ang_bank)));
+vmax=sqrt((r*g*(tand(ang_bank)+TyreFriction))/(1-TyreFriction*tand(ang_bank)));
 
 end
